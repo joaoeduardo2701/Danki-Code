1ca2a8c3eddb4ea97542f7ddde21b84829df0f72
Eduardo Padilha <63940232+joaoeduardo2701@users.noreply.github.com>
2022-12-20 20:04:54 -0300

    Retirando comentários desnecessários

--- Exercícios de JS DankiCode/Aula 07 - DOM/script.js
+++ Exercícios de JS DankiCode/Aula 07 - DOM/script.js
@@ -1,6 +1,6 @@
 var a = window.document.getElementById('area')
 
-a.addEventListener('click', clicar)
+a.addEventListener('click', clicar) 
 a.addEventListener('mouseenter', mouseEntrou)
 a.addEventListener('mouseout', mouseSaiu)
 
@@ -18,33 +18,3 @@ function mouseSaiu(){
     a.innerHTML = 'Saiu'
     a.style.backgroundColor = 'red'
 }
-
-
-
-
-
-
-
-
-
-
-/*
-a.addEventListener('click', clicar)
-a.addEventListener('mouseenter', mouseEntrou)
-a.addEventListener('mouseout', mouseSaiu)
-
-function clicar(){
-    a.innerText = "Clicou"
-    a.style.backgroundColor = "black"
-}
-
-function mouseEntrou(){
-    a.innerText = 'Entrou'
-    a.style.backgroundColor = "green"
-}
-
-function mouseSaiu(){
-    a.innerText = 'Saiu'
-    a.style.backgroundColor = "red"
-}
-*/
